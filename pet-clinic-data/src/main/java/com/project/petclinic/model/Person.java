7e93fad1fd76657d7ae5a78293ff9546f11a25d7
ZZ02SR826 <nicuavram11@gmail.com>
2022-01-30 21:55:52 +0200

    Implemented base entity for model classes.

--- pet-clinic-data/src/main/java/com/project/petclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/project/petclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.project.petclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/com/project/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/project/petclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.project.petclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
     private String name;
 
     public String getName() {
