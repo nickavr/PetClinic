7f4f8ae1e3949bbcf26ed7947279cf65da10cd5c
ZZ02SR826 <nicuavram11@gmail.com>
2022-01-30 21:45:50 +0200

    Added service interfaces. Closes #5

---
+++ pet-clinic-data/src/main/java/com/project/petclinic/services/PetService.java
@@ -0,0 +1,13 @@
+package com.project.petclinic.services;
+
+import com.project.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
