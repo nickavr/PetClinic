737dd24a6b22b85fadf3242b2fbbd4878aedd27e
ZZ02SR826 <nicuavram11@gmail.com>
2022-03-02 22:21:34 +0200

    added list of vets. closes #13

--- pet-clinic-data/src/main/java/com/project/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/project/petclinic/services/VetService.java
@@ -2,7 +2,5 @@ package com.project.petclinic.services;
 
 import com.project.petclinic.model.Vet;
 
-import java.util.Set;
-
 public interface VetService extends CrudService<Vet, Long>{
 }
--- pet-clinic-web/src/main/java/com/project/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/project/petclinic/controllers/VetController.java
@@ -1,13 +1,23 @@
 package com.project.petclinic.controllers;
 
+import com.project.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"}) //multiple options for the route
-    public String listVets(){
+    public String listVets(Model model){
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
